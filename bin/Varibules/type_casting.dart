c060d70e5ceb11dee5034a2ca72f468845348697
Ahmed Khaled <50336331+darrrwish@users.noreply.github.com>
2025-04-24 18:12:11 +0200

    Update type_casting.dart

--- bin/Varibules/type_casting.dart
+++ bin/Varibules/type_casting.dart
@@ -12,14 +12,14 @@ void main() {
   //!تحويل من متغير لمتغير
   int salary = 3800;
   double egtax = 0.14;
-  String infactionTax = '160.26';
+  String infectionTax = '160.26';
   double totalSalary =
       salary.toDouble() -
-      egtax +
-      double.parse(infactionTax); // تحويل الرقم الصحيح و النص الي رقم عشري
+      egtax + 
+      double.parse(infectionTax); // تحويل الرقم الصحيح و النص الي رقم عشري
 
   print(
-    'الراتب: $totalSalary  مع اضافة بدل مكافحة عدوى  $infactionTax جنيه مصري و خصم القيمة المضافة $egtax جنيه مصري',
+    'الراتب: $totalSalary  مع اضافة بدل مكافحة عدوى  $infectionTax جنيه مصري و خصم القيمة المضافة $egtax جنيه مصري',
   );
 
 
